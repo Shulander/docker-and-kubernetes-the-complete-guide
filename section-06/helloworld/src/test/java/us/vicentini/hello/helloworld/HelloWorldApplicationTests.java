9be6cf7b5b68bfe7b7a3f7c57a8066cbb97a53d3
Henrique Vicentini <Henrique_A_Vicentini@gap.com>
2023-11-26 09:09:59 -0800

    adding a spring boot app because node sucks

---
+++ section-06/helloworld/src/test/java/us/vicentini/hello/helloworld/HelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package us.vicentini.hello.helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
